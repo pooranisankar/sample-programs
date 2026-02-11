81ffd9b28fd646252915aa6e7d3c1b4db7f1a3d8
pooranisankar <pooranisankar888@gmail.com>
2026-02-11 22:01:18 +0530

    Update StringReverse.cpp

--- StringReverse.cpp
+++ StringReverse.cpp
@@ -1,19 +1,33 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 using namespace std;
-int main(){
-    string s;
-    cout<<"enter a string: ";
-    getline(cin,s);
-    cout<<"reversed string is: ";
-    for(int i=s.length()-1;i>=0;i--){
-        cout<<s[i]; 
-    }        
+
+int main() {
+    char choice;
+
+    do {
+        string s, rev = "";
+
+        cout << "Enter a string to reverse: ";
+        cin >> s;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            rev += s[i];
+        }
+
+        cout << "Reversed string is: " << rev << endl;
+
+        cout << "Do you want to continue (Y/N): ";
+        cin >> choice;
+
+        while (choice != 'Y' && choice != 'y' &&
+               choice != 'N' && choice != 'n') {
+            cout << "Invalid entry! Do you want to continue (Y/N): ";
+            cin >> choice;
+        }
+
+    } while (choice == 'Y' || choice == 'y');
+
+    cout << "Program ended.";
     return 0;
 }
-
-
-
-
-
-/* reverse a string program*/
