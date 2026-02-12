9c433c8a865ef20623ba34033e3bfc519c8765f2
pooranisankar <pooranisankar888@gmail.com>
2026-02-12 22:49:47 +0530

    Update OddOrEven.cpp

--- OddOrEven.cpp
+++ OddOrEven.cpp
@@ -1,22 +1,35 @@
-#include<iostream>
-#include<string>
+
+#include <iostream>
 using namespace std;
-int main(){
-    int number;
-    cout<<"enter the number:";
-    cin>>number;
-    if(number%2==0){
-        cout<<"it is a even number";
-    }
-    else
-    {
-        cout<<"it is a odd number";
-    }
-        
-    
+
+int main() {
+    char choice;
+
+    do {
+        int number;
+
+        cout << "Enter a number: ";
+        cin >> number;
+
+        if (number % 2 == 0) {
+            cout << "It is an even number." << endl;
+        }
+        else {
+            cout << "It is an odd number." << endl;
+        }
+
+        cout << "Do you want to continue (Y/N): ";
+        cin >> choice;
+
+        while (choice != 'Y' && choice != 'y' &&
+               choice != 'N' && choice != 'n') {
+            cout << "Invalid entry! Do you want to continue (Y/N): ";
+            cin >> choice;
+        }
+
+    } while (choice == 'Y' || choice == 'y');
+
+    cout << "Program ended.";
+    return 0;
 }
-
-
-
-
 /* find whether the given number is even or odd*/
