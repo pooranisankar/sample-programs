fef204cb8d5ab6ec594c2d543c27ee4eee6b2b1d
pooranisankar <pooranisankar888@gmail.com>
2026-02-12 20:33:25 +0530

    Update PalindromeOrNot.cpp

--- PalindromeOrNot.cpp
+++ PalindromeOrNot.cpp
@@ -1,20 +1,41 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 using namespace std;
-int main(){
-    string s,reverse="";
-    cout<<"enter a string:";
-    getline(cin,s);
-    for(int i=s.length()-1;i>=0;i--){
-        reverse=reverse+s[i];
-    }
-    if(s==reverse){
-        cout<<"it is a palindrome";
-    }
-    else{
-        cout<<"its is not a palindrome"; 
-    }
 
+int main() {
+    char choice;
+
+    do {
+        string s, rev = "";
+
+        cout << "Enter a string: ";
+        cin >> s;
+
+        // Reverse logic
+        for (int i = s.length() - 1; i >= 0; i--) {
+            rev += s[i];
+        }
+
+        // Palindrome check
+        if (s == rev) {
+            cout << "It is a Palindrome." << endl;
+        } else {
+            cout << "It is NOT a Palindrome." << endl;
+        }
+
+        cout << "Do you want to continue (Y/N): ";
+        cin >> choice;
+
+        while (choice != 'Y' && choice != 'y' &&
+               choice != 'N' && choice != 'n') {
+            cout << "Invalid entry! Do you want to continue (Y/N): ";
+            cin >> choice;
+        }
+
+    } while (choice == 'Y' || choice == 'y');
+
+    cout << "Program ended.";
+    return 0;
 }
 
 
