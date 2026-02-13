bf951e9a5d7e384c466bb5c57838c2554057df70
pooranisankar <pooranisankar888@gmail.com>
2026-02-13 19:47:26 +0530

    Update PrimeOrComposite.cpp

--- PrimeOrComposite.cpp
+++ PrimeOrComposite.cpp
@@ -1,23 +1,40 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    int n,count=0;
-    cout<<"enter a number:";
-    cin>>n;
-    for(int i=1;i<=n;i++){
-        if(n%i==0){
-            count++;
+
+int main() {
+    char choice;
+
+    do {
+        int n, count = 0;
+
+        cout << "Enter a number: ";
+        cin >> n;
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                count++;
+            }
         }
-    } 
-    if(count==2)
-        cout<<"It is a prime number";
-    
-    else
-        cout<<"It is a composite number";
-    
-   
-     
-}            
+
+        if (count == 2)
+            cout << "It is a prime number" << endl;
+        else
+            cout << "It is a composite number" << endl;
+
+        cout << "Do you want to continue (Y/N): ";
+        cin >> choice;
+
+        while (choice != 'Y' && choice != 'y' &&
+               choice != 'N' && choice != 'n') {
+            cout << "Invalid entry! Do you want to continue (Y/N): ";
+            cin >> choice;
+        }
+
+    } while (choice == 'Y' || choice == 'y');
+
+    cout << "Program ended.";
+    return 0;
+}
 
 
 
