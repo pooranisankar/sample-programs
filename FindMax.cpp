8fa5caa20d9a98a3c7fc1ecdcbcecb2b5bcdb8e7
pooranisankar <pooranisankar888@gmail.com>
2026-02-13 20:22:45 +0530

    Update FindMax.cpp

--- FindMax.cpp
+++ FindMax.cpp
@@ -1,19 +1,36 @@
-#include<iostream>
+
+#include <iostream>
 using namespace std;
-int main(){
-    int a[6]={9,5,7,3,8,2};
-    int max=a[0];
-    for(int i=1;i<6;i++){
-        if(a[i]>max){
-            max=a[i];
+
+int main() {
+    char choice;
+
+    do {
+        int a[6] = {9, 5, 7, 3, 8, 2};
+        int max = a[0];
+
+        for (int i = 1; i < 6; i++) {
+            if (a[i] > max) {
+                max = a[i];
+            }
         }
-    }
-    cout<<"maximum  number is:"<<max;
+
+        cout << "Maximum number is: " << max << endl;
+
+        cout << "\nDo you want to continue (Y/N): ";
+        cin >> choice;
+
+        while (choice != 'Y' && choice != 'y' &&
+               choice != 'N' && choice != 'n') {
+            cout << "Invalid entry! Do you want to continue (Y/N): ";
+            cin >> choice;
+        }
+
+    } while (choice == 'Y' || choice == 'y');
+
+    cout << "Program ended.";
     return 0;
-
-
 }
-
 
 
 /* find the maximum number in an array*/
