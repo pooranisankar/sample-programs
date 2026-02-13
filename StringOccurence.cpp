389ed4d3774124587a5d0f1a427bb4f7b583c5e2
pooranisankar <pooranisankar888@gmail.com>
2026-02-13 20:50:41 +0530

    Update StringOccurence.cpp

--- StringOccurence.cpp
+++ StringOccurence.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
     char choice;
 
     do {
-        int a[10] = {3, 6, 8, 9, 2, 7, 2, 5, 2, 2};
+        string str = "dare devil";
+        char ch = 'd';
         int count = 0;
 
-        for (int i = 0; i < 10; i++) {
-            if (a[i] == 2) {
+        for (int i = 0; i < str.length(); i++) {
+            if (str[i] == ch) {
                 count++;
             }
         }
 
-        cout << "Occurrence of 2 is: " << count << endl;
+        cout << "Occurrence of '" << ch << "' in string is: " << count << endl;
 
         cout << "\nDo you want to continue (Y/N): ";
         cin >> choice;
@@ -30,7 +32,4 @@ int main() {
     cout << "Program ended.";
     return 0;
 }
-
-
-
-/* count the occurrence of 2 in an array and ask user whether to continue or not*/
+/* count the occurrence of a character in a string and ask user whether to continue or not*/
