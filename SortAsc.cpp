b35f732e9696bea08f66fa9fe73c17dc57db5742
pooranisankar <pooranisankar888@gmail.com>
2026-02-13 20:02:06 +0530

    Update SortAsc.cpp

--- SortAsc.cpp
+++ SortAsc.cpp
@@ -1,24 +1,45 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    int a[6]={9,5,7,3,8,2};
-    int temp;
-    for(int i=0;i<6;i++){
-        for(int j=i+1;j<6;j++){
-            if(a[i]>a[j]){
-                temp=a[i];
-                a[i]=a[j];
-                a[j]=temp;
-                
+
+int main() {
+    char choice;
+
+    do {
+        int a[6] = {9, 5, 7, 3, 8, 2};
+        int temp;
+
+        // Sorting (Inner logic loop)
+        for (int i = 0; i < 6; i++) {
+            for (int j = i + 1; j < 6; j++) {
+                if (a[i] > a[j]) {
+                    temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
             }
         }
-    }
-    cout<<"sorted array is:";
-    for(int i=0;i<6;i++){
-        cout<<a[i]<<" ";    
-    }
+
+        cout << "Sorted array is: ";
+        for (int i = 0; i < 6; i++) {
+            cout << a[i] << " ";
+        }
+        cout << endl;
+
+        cout << "\nDo you want to continue (Y/N): ";
+        cin >> choice;
+
+        while (choice != 'Y' && choice != 'y' &&
+               choice != 'N' && choice != 'n') {
+            cout << "Invalid entry! Do you want to continue (Y/N): ";
+            cin >> choice;
+        }
+
+    } while (choice == 'Y' || choice == 'y');
+
+    cout << "Program ended.";
     return 0;
 }
+
 
 
 
