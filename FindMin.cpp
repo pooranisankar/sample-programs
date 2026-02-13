d5160df95ed77004ba38e586f7a389d11e0ec7ec
pooranisankar <pooranisankar888@gmail.com>
2026-02-13 20:26:14 +0530

    Update FindMin.cpp

--- FindMin.cpp
+++ FindMin.cpp
@@ -1,18 +1,36 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    int a[6]={9,5,7,3,8,2};
-    int min=a[0];
-    for(int i=1;i<6;i++){
-        if(a[i]<min){
-             min=a[i];
+
+int main() {
+    char choice;
+
+    do {
+        int a[6] = {9, 5, 7, 3, 8, 2};
+        int min = a[0];
+
+        for (int i = 1; i < 6; i++) {
+            if (a[i] < min) {   
+                min = a[i];
+            }
         }
-    }
-    cout<<"minimum number is:"<<min;
+
+        cout << "Minimum number is: " << min << endl;
+
+        cout << "\nDo you want to continue (Y/N): ";
+        cin >> choice;
+
+        while (choice != 'Y' && choice != 'y' &&
+               choice != 'N' && choice != 'n') {
+            cout << "Invalid entry! Do you want to continue (Y/N): ";
+            cin >> choice;
+        }
+
+    } while (choice == 'Y' || choice == 'y');
+
+    cout << "Program ended.";
     return 0;
-
-
 }
+
 
 
 
