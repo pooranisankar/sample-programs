523cea5a8eaf70970cdb4b57f4c48070d8f80230
pooranisankar <pooranisankar888@gmail.com>
2026-03-01 19:51:04 +0530

    Update ArrayOccurence.cpp

--- ArrayOccurence.cpp
+++ ArrayOccurence.cpp
@@ -5,32 +5,39 @@ int main() {
     char choice;
 
     do {
-        int a[10] = {3, 6, 8, 9, 2, 7, 2, 5, 2, 2};
-        int count = 0;
+        int n,value;
+        cout<<"enter size of array:";
+        cin>>n;
 
-        for (int i = 0; i < 10; i++) {
-            if (a[i] == 2) {
+        int a[n];
+        cout<<"enter array elements:";
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        cout<<"enter the value to find occurrence:";
+        cin>>value;
+
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(a[i]==value){
                 count++;
             }
         }
 
-        cout << "Occurrence of 2 is: " << count << endl;
+        cout << "Occurrence of " << value << " is: " << count << endl;
 
         cout << "\nDo you want to continue (Y/N): ";
         cin >> choice;
-
-        while (choice != 'Y' && choice != 'y' &&
-               choice != 'N' && choice != 'n') {
-            cout << "Invalid entry! Do you want to continue (Y/N): ";
-            cin >> choice;
+        
+        if(choice =='N'|| choice == 'n'){
+            cout<<"Program ended"<<endl;
         }
-
+        else if(choice != 'Y' && choice !='y'){
+            cout<<"Invalid entry!"<<endl;
+        }
     } while (choice == 'Y' || choice == 'y');
 
-    cout << "Program ended.";
+ 
     return 0;
 }
-
-
-
 /* count the occurrence of 2 in an array and ask user whether to continue or not*/
