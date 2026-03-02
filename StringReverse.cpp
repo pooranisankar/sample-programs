58a4984d0b702648457ea5fa421d1acb5e5732cb
pooranisankar <pooranisankar888@gmail.com>
2026-03-02 12:44:15 +0530

    Update StringReverse.cpp

--- StringReverse.cpp
+++ StringReverse.cpp
@@ -8,8 +8,8 @@ int main() {
     do {
         string s, rev = "";
 
-        cout << "Enter a string to reverse: ";
-        cin >> s;
+        cout << "Enter a string: ";
+        getline(cin, s);
 
         for (int i = s.length() - 1; i >= 0; i--) {
             rev += s[i];
@@ -20,14 +20,17 @@ int main() {
         cout << "Do you want to continue (Y/N): ";
         cin >> choice;
 
-        while (choice != 'Y' && choice != 'y' &&
-               choice != 'N' && choice != 'n') {
-            cout << "Invalid entry! Do you want to continue (Y/N): ";
-            cin >> choice;
+        if (choice != 'Y' && choice != 'y' &&
+            choice != 'N' && choice != 'n') {
+            cout << "Invalid entry!" << endl;
+            break;  
         }
+
+        cin.ignore(1000, '\n');  
 
     } while (choice == 'Y' || choice == 'y');
 
-    cout << "Program ended.";
+    cout << "Program ended." << endl;
     return 0;
 }
+/* reverse a string and ask user whether to continue or not*/
